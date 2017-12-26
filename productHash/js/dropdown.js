d85c722023fd4e9d104fe333fac9ab288570f442
jynti <33417806+jynti@users.noreply.github.com>
2017-12-26 16:43:14 +0530

    Update dropdown.js

--- productHash/js/dropdown.js
+++ productHash/js/dropdown.js
@@ -1,117 +1,92 @@
-function Dropdown(name, optionValues, domElement) {
-  this.name = name;
-  this.optionValues = optionValues;
-  this.sideFilter = domElement.sideFilters;
-  this.productContentArea = domElement.productContentArea;
-  this.url = domElement.url;
-  this.footer = domElement.footer;
-  this.allProducts = domElement.allProducts;
+function SortPageDropdown(options, domDetails) {
+  this.sideFilter = domDetails.sideFilters;
+  this.productContentArea = domDetails.productContentArea;
+  this.url = domDetails.url;
+  this.footer = domDetails.footer;
+  this.allProducts = domDetails.allProducts;
+  this.options = options;
+  this.selectedValues = {};
 }
 
-Dropdown.prototype.init = function() {
-  this.createOptions();
+SortPageDropdown.prototype.init = function() {
+  this.createDropdowns();
 }
 
-Dropdown.prototype.createOptions = function() {
+SortPageDropdown.prototype.createDropdowns = function() {
+  var _this = this;
+  this.options.forEach(function(dropdownType) {
+    _this.createOptions(dropdownType);
+    _this.selectedValues[dropdownType.name] = dropdownType.options[0][0];
+  });
+}
+
+SortPageDropdown.prototype.createOptions = function(dropdownType) {
   var selectBox = $("<select></select>").addClass("select-box");
-
-  for (var optionIndex = 0; optionIndex < this.optionValues.length; optionIndex++) {
-    var option = $("<option></option>").attr("value", this.optionValues[optionIndex][0]).text(this.optionValues[optionIndex][1]);
+  var name = dropdownType.name;
+  var dropdownOptions = dropdownType.options;
+  for (var optionIndex = 0; optionIndex < dropdownOptions.length; optionIndex++) {
+    var option = $("<option></option>").attr("value", dropdownOptions[optionIndex][0]).text(dropdownOptions[optionIndex][1]);
     if (optionIndex == 0) {
       option.prop("selected", true);
     }
     selectBox.append(option);
   }
-
-  var _this = this;
-  selectBox.on("change", function() {
-    _this.selectedValue = $(this).val();
-    _this.onSelection();
-  });
-
+  this.onChangeEvent(selectBox, name);
   this.sideFilter.append(selectBox);
   this.sideFilter.append("<hr>");
 }
 
-Dropdown.prototype.onSelection = function() {
-  if (this.name == "pages") {
-    this.constructor.selectedValue = this.selectedValue;
-  } else {
-    this.constructor.sortSelectedValue = this.selectedValue;
-  }
-
-  this.url[this.name] = this.selectedValue;
-  window.location.hash = JSON.stringify(this.url);
-  this.display()
+SortPageDropdown.prototype.onChangeEvent = function(selectBox, name) {
+  var _this = this;
+  selectBox.on("change", function() {
+    _this.selectedValues[name] = $(this).val();
+    _this.url[name] = $(this).val();
+    window.location.hash = JSON.stringify(_this.url);
+    _this.display();
+  });
 }
 
-Dropdown.prototype.display = function() {
+SortPageDropdown.prototype.display = function() {
   this.visibleProducts = Product.getVisibleProducts();
   if (!this.visibleProducts) this.visibleProducts = this.allProducts;
-  if (this.name == "pages") {
-    this.createFooter();
-    Product.show(0, this.selectedValue - 1, this.visibleProducts, this.productContentArea);
-  } else {
-    this.createFooter();
-    var selectedPagination = Dropdown.getPresentlySelectedValue();
-    Product.show(0, selectedPagination - 1, this.visibleProducts, this.productContentArea);
-  }
+  this.createFooter();
+  var selectedPagination = this.selectedValues["pages"];
+  Product.show(0, selectedPagination - 1, this.visibleProducts, this.productContentArea, this);
 }
 
-Dropdown.onSortClickEvent = function(selectedValue, visibleProducts) {
-  this.sortSelectedValue = selectedValue;
+SortPageDropdown.prototype.onSortClickEvent = function(selectedValue, visibleProducts) {
   if (selectedValue == "name" || selectedValue == "available") {
-    visibleProducts = this.sortForNumbers(selectedValue, visibleProducts);
+    visibleProducts = this.sortByNumbers(selectedValue, visibleProducts);
   } else {
-    visibleProducts = this.sortForWord(selectedValue, visibleProducts);
+    visibleProducts = this.sortByWord(selectedValue, visibleProducts);
   }
   return visibleProducts;
 }
 
-Dropdown.sortForNumbers = function(selectedValue, visibleProducts) {
+SortPageDropdown.prototype.sortByNumbers = function(selectedValue, visibleProducts) {
   visibleProducts.sort(function(product1, product2) {
     return product1[selectedValue] - product2[selectedValue];
   });
   return visibleProducts;
 }
 
-Dropdown.sortForWord = function(selectedValue, visibleProducts) {
+SortPageDropdown.prototype.sortByWord = function(selectedValue, visibleProducts) {
   visibleProducts.sort(function(product1, product2) {
-    if (product1[selectedValue].toUpperCase() < product2[selectedValue].toUpperCase()) {
-      return -1;
-    }
-    if (product1[selectedValue].toUpperCase() > product2[selectedValue].toUpperCase()) {
-      return 1;
-    }
-    return 0;
+    return product1[selectedValue].toUpperCase() > product2[selectedValue].toUpperCase()
   });
   return visibleProducts;
 }
 
-Dropdown.prototype.createFooter = function() {
+SortPageDropdown.prototype.createFooter = function() {
   var domDetails = {
     footer: this.footer,
     productContentArea: this.productContentArea,
-    url: this.url
+    url: this.url,
+    dropdown: this
   }
-
-  var footer = new Footer(this.visibleProducts, Dropdown.getPresentlySelectedValue(), domDetails);
+  var footer = new Footer(this.visibleProducts, this.selectedValues["pages"], domDetails);
   footer.init();
+  footer.highlightButton(0);
   this.url["pageNumber"] = "0";
   window.location.hash = JSON.stringify(this.url);
-  footer.highlightButton(0);
-}
-
-Dropdown.getPresentlySelectedValue = function() {
-  if (!this.selectedValue) {
-    this.selectedValue = "20"
-  }
-  return this.selectedValue;
-}
-
-Dropdown.getPresentlySelectedSortValue = function() {
-  if (!this.sortSelectedValue) {
-    this.sortSelectedValue = "name"
-  }
-  return this.sortSelectedValue;
 }
